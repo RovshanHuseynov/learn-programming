5028d88f8c4b6545f6c5ff5686fe8eef9ab10356
rovshan.huseynov <rovsen.huseyn.oglu@gmail.com>
2022-11-16 20:53:31 +0400

    -- improve binarySearch method

--- src/main/java/lesson0/binarySearch/Main.java
+++ src/main/java/lesson0/binarySearch/Main.java
@@ -14,22 +14,40 @@ public class Main {
             arr[i] = in.nextInt();
         }
         Arrays.sort(arr);
-        System.out.println("10: " + binarySearch(0, arr.length - 1, 10, arr));
-        System.out.println("1: " + binarySearch(0, arr.length - 1, 1, arr));
-        System.out.println("5: " + binarySearch(0, arr.length - 1, 5, arr));
-        System.out.println("6: " + binarySearch(0, arr.length - 1, 6, arr));
-        System.out.println("11: " + binarySearch(0, arr.length - 1, 11, arr));
-        System.out.println("13: " + binarySearch(0, arr.length - 1, 13, arr));
-        System.out.println("-5: " + binarySearch(0, arr.length - 1, -5, arr));
-        System.out.println("8: " + binarySearch(0, arr.length - 1, 8, arr));
+        System.out.println("10: " + binarySearchContains(0, arr.length - 1, 10, arr));
+        System.out.println("1: " + binarySearchContains(0, arr.length - 1, 1, arr));
+        System.out.println("5: " + binarySearchContains(0, arr.length - 1, 5, arr));
+        System.out.println("6: " + binarySearchContains(0, arr.length - 1, 6, arr));
+        System.out.println("11: " + binarySearchContains(0, arr.length - 1, 11, arr));
+        System.out.println("13: " + binarySearchContains(0, arr.length - 1, 13, arr));
+        System.out.println("-5: " + binarySearchContains(0, arr.length - 1, -5, arr));
+        System.out.println("8: " + binarySearchContains(0, arr.length - 1, 8, arr));
+        System.out.println("-------------------------------------");
+        System.out.println("index of5 10: " + binarySearchFindIndex(0, arr.length - 1, 10, arr));
+        System.out.println("index of 1: " + binarySearchFindIndex(0, arr.length - 1, 1, arr));
+        System.out.println("index of 5: " + binarySearchFindIndex(0, arr.length - 1, 5, arr));
+        System.out.println("index of 6: " + binarySearchFindIndex(0, arr.length - 1, 6, arr));
+        System.out.println("index of 11: " + binarySearchFindIndex(0, arr.length - 1, 11, arr));
+        System.out.println("index of 13: " + binarySearchFindIndex(0, arr.length - 1, 13, arr));
+        System.out.println("index of -5: " + binarySearchFindIndex(0, arr.length - 1, -5, arr));
+        System.out.println("index of 8: " + binarySearchFindIndex(0, arr.length - 1, 8, arr));
     }
 
-    public static boolean binarySearch(int left, int right, int target, int [] arr){
+    public static boolean binarySearchContains(int left, int right, int target, int [] arr){
         if(left > right || left < 0 || right >= arr.length) return false;
         int mid = (left + right) / 2;
 
         if(arr[mid] == target) return true;
-        else if (arr[mid] < target) return binarySearch(mid+1, right, target, arr);
-        else return binarySearch(left, mid-1, target, arr);
+        else if (arr[mid] < target) return binarySearchContains(mid+1, right, target, arr);
+        else return binarySearchContains(left, mid-1, target, arr);
+    }
+
+    public static int binarySearchFindIndex(int left, int right, int target, int [] arr){
+        if(left > right || left < 0 || right >= arr.length) return -1;
+        int mid = (left + right) / 2;
+
+        if(arr[mid] == target) return mid;
+        else if (arr[mid] < target) return binarySearchFindIndex(mid+1, right, target, arr);
+        else return binarySearchFindIndex(left, mid-1, target, arr);
     }
 }
