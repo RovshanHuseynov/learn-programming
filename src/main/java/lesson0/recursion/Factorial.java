489e6c2b7a43a76c7e95be537277f22224d4bcd8
rovshan.huseynov <rovsen.huseyn.oglu@gmail.com>
2022-11-20 11:42:51 +0400

    -- add BigInteger to factorial() method

--- src/main/java/lesson0/recursion/Factorial.java
+++ src/main/java/lesson0/recursion/Factorial.java
@@ -1,14 +1,16 @@
 package lesson0.recursion;
 
+import java.math.BigInteger;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Factorial {
     Map<Integer, Integer> memoization = new HashMap<>();
 
-    public long factorialRecursion(long n){
-        if(n < 0) throw new IllegalStateException("input value must be a positive integer");
-        if(n == 0 || n == 1) return 1;
-        return n * factorialRecursion(n - 1);
+    public BigInteger factorialRecursion(BigInteger n){
+        if(n.compareTo(BigInteger.ZERO) < 0)
+            throw new IllegalStateException("input value must be a positive integer");
+        if(n.equals(BigInteger.ZERO)) return BigInteger.ONE;
+        return n.multiply(factorialRecursion(n.subtract(BigInteger.ONE)));
     }
 }
--- src/main/java/lesson0/recursion/Main.java
+++ src/main/java/lesson0/recursion/Main.java
@@ -1,5 +1,7 @@
 package lesson0.recursion;
 
+import java.math.BigInteger;
+
 public class Main {
     public static void main(String[] args) {
         /*Fibonacci fibo = new Fibonacci();
@@ -18,12 +20,12 @@ public class Main {
 
         Factorial f = new Factorial();
         //System.out.println(f.factorialRecursion(-10));
-        System.out.println(f.factorialRecursion(1));
-        System.out.println(f.factorialRecursion(3));
-        System.out.println(f.factorialRecursion(4));
-        System.out.println(f.factorialRecursion(20));
-        System.out.println(f.factorialRecursion(50));
-        System.out.println(f.factorialRecursion(100));
-        System.out.println(f.factorialRecursion(500));
+        System.out.println("Factorial 1: " + f.factorialRecursion(new BigInteger("1")));
+        System.out.println("Factorial 5: " + f.factorialRecursion(new BigInteger("5")));
+        System.out.println("Factorial 20: " + f.factorialRecursion(new BigInteger("20")));
+        System.out.println("Factorial 50: " + f.factorialRecursion(new BigInteger("50")));
+        System.out.println("Factorial 200: " + f.factorialRecursion(new BigInteger("200")));
+        System.out.println("Factorial 1000: " + f.factorialRecursion(new BigInteger("1000")));
+        System.out.println("Factorial 5000: " + f.factorialRecursion(new BigInteger("5000")));
     }
 }
