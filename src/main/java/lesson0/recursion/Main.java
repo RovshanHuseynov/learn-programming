25f2dd4aa44ab005195bee52f84803d6e27426cf
rovshan.huseynov <rovsen.huseyn.oglu@gmail.com>
2022-11-25 22:57:17 +0400

    -- minor changes

--- src/main/java/lesson0/recursion/Main.java
+++ src/main/java/lesson0/recursion/Main.java
@@ -30,5 +30,6 @@ public class Main {
         System.out.println(p.power(5,5));
         System.out.println(p.power(8,4));
         System.out.println(p.power(6,4));
+        System.out.println(p.power(7,3));
     }
 }
