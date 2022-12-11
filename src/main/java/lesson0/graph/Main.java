707e5712095a05835e7a8d95c9552fc32a27d10d
rovshan.huseynov <rovsen.huseyn.oglu@gmail.com>
2022-12-11 10:17:40 +0400

    -- improve quickSort() method

--- src/main/java/lesson0/graph/Main.java
+++ src/main/java/lesson0/graph/Main.java
@@ -1,7 +1,11 @@
 package lesson0.graph;
 
+import lesson0.sorting.QuickSort;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        int[] array = {1,2,3,0,-1,-3,-2};
+        QuickSort qs = new QuickSort();
+        qs.quickSort(array, 0, array.length);
     }
 }
