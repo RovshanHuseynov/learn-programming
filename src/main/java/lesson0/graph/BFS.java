b16f047645042bd75ba15fa10a45e3aa133527e7
rovshan.huseynov <rovsen.huseyn.oglu@gmail.com>
2022-12-14 23:26:36 +0400

    -- improve DFS and BFS

--- src/main/java/lesson0/graph/BFS.java
+++ src/main/java/lesson0/graph/BFS.java
@@ -4,20 +4,41 @@ import java.util.Scanner;
 
 public class BFS {
     // source: https://www.youtube.com/watch?v=pcKY4hjDrxk
+    /*
+    input:
+    7 6
+    1 4
+    1 5
+    1 2
+    2 7
+    2 6
+    2 3
+    */
+    static int[][] arr;
+    static boolean[] used;
+    static int n,m;
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        int m = in.nextInt();
-        int[][] arr = new int[n][m];
+        n = in.nextInt();
+        m = in.nextInt();
+        arr = new int[n+1][n+1];
+        used = new boolean[n+1];
         for(int i=0; i<m; i++){
             int x = in.nextInt();
             int y = in.nextInt();
             arr[x][y] = 1;
             arr[y][x] = 1;
         }
+        bfs(1);
     }
 
     public static void bfs(int node){
-
+        for(int i=1; i<=n; i++){
+            if(!used[i] && arr[node][i] == 1){
+                used[i] = true;
+                System.out.print(i + " ");
+                bfs(node);
+            }
+        }
     }
 }
