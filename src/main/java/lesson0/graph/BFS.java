7d5611d1553205253cb053646e040a06d9d87a93
rovshan.huseynov <rovsen.huseyn.oglu@gmail.com>
2022-12-14 23:15:36 +0400

    -- implement DFS and BFS algorithm

--- src/main/java/lesson0/graph/BFS.java
+++ src/main/java/lesson0/graph/BFS.java
@@ -1,4 +1,23 @@
 package lesson0.graph;
 
+import java.util.Scanner;
+
 public class BFS {
+    // source: https://www.youtube.com/watch?v=pcKY4hjDrxk
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int m = in.nextInt();
+        int[][] arr = new int[n][m];
+        for(int i=0; i<m; i++){
+            int x = in.nextInt();
+            int y = in.nextInt();
+            arr[x][y] = 1;
+            arr[y][x] = 1;
+        }
+    }
+
+    public static void bfs(int node){
+
+    }
 }
--- src/main/java/lesson0/graph/DFS.java
+++ src/main/java/lesson0/graph/DFS.java
@@ -1,4 +1,24 @@
 package lesson0.graph;
 
+import java.util.Scanner;
+
 public class DFS {
+    // source: https://www.youtube.com/watch?v=pcKY4hjDrxk
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int m = in.nextInt();
+        int[][] arr = new int[n][m];
+        for(int i=0; i<m; i++){
+            int x = in.nextInt();
+            int y = in.nextInt();
+            arr[x][y] = 1;
+            arr[y][x] = 1;
+        }
+
+    }
+
+    public static void dfs(int node){
+
+    }
 }
