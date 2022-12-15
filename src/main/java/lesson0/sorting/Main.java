a7ef86131387b6e71c0d0e3ea8321f0d787f7f11
rovshan.huseynov <rovsen.huseyn.oglu@gmail.com>
2022-12-15 22:17:47 +0400

    -- add QuickSort sorting

--- src/main/java/lesson0/sorting/Main.java
+++ src/main/java/lesson0/sorting/Main.java
@@ -1,9 +1,13 @@
 package lesson0.sorting;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        int[] array = {1,2,3,0,-1,-3,-2};
+        int[] array = {1,2,3,0,-1,-3,-2, Integer.MAX_VALUE};
+        System.out.println(Arrays.toString(array));
         QuickSort qs = new QuickSort();
         qs.quickSort(array, 0, array.length);
+        System.out.println(Arrays.toString(array));
     }
 }
--- src/main/java/lesson0/sorting/QuickSort.java
+++ src/main/java/lesson0/sorting/QuickSort.java
@@ -5,11 +5,35 @@ public class QuickSort {
         if(l>=r) return;
 
         int pivot = partition(arr,l,r);
-        quickSort(arr, l, pivot-1);
+        quickSort(arr, l, pivot);
         quickSort(arr, pivot+1, r);
     }
 
     private int partition(int[] arr, int l, int r){
-        return 0;
+        int pivot  = arr[l];
+        int i = l;
+        int j = r;
+
+        while (i < j) {
+            do {
+                i++;
+            } while (arr[i] <= pivot);
+
+            do {
+                j--;
+            } while (arr[j] >= pivot);
+
+            if (i < j) {
+                // swapping
+                arr[i] = arr[i] + arr[j];
+                arr[j] = arr[i] - arr[j];
+                arr[i] = arr[i] - arr[j];
+            }
+        }
+        // swapping
+        arr[l] = arr[l] + arr[j];
+        arr[j] = arr[l] - arr[j];
+        arr[l] = arr[l] - arr[j];
+        return j;
     }
 }
