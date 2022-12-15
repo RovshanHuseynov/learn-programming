3bec7c22323256d4668393438280ebbf82b4d266
rovshan.huseynov <rovsen.huseyn.oglu@gmail.com>
2022-12-15 20:50:02 +0400

    -- improve BFS with having queue data structure

--- src/main/java/lesson0/graph/BFS.java
+++ src/main/java/lesson0/graph/BFS.java
@@ -1,5 +1,7 @@
 package lesson0.graph;
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class BFS {
@@ -29,15 +31,20 @@ public class BFS {
             arr[x][y] = 1;
             arr[y][x] = 1;
         }
-        bfs(1);
+        bfs();
     }
 
-    public static void bfs(int node){
-        for(int i=1; i<=n; i++){
-            if(!used[i] && arr[node][i] == 1){
-                used[i] = true;
-                System.out.print(i + " ");
-                bfs(node);
+    public static void bfs(){
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(1);
+
+        while (!queue.isEmpty()){
+            int top = queue.poll();
+            for(int i=1; i<=n; i++){
+                if(!used[i] && arr[top][i] == 1){
+                    queue.add(i);
+                    used[i] = true;
+                }
             }
         }
     }
