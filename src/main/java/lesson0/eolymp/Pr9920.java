4aa6853469a88e040cd3086fcf4724467e2b1ce6
rovshan.huseynov <rovsen.huseyn.oglu@gmail.com>
2023-01-21 23:03:22 +0400

    -- solve eolymp problem 9920

--- src/main/java/lesson0/eolymp/Pr9920.java
+++ src/main/java/lesson0/eolymp/Pr9920.java
@@ -2,7 +2,7 @@ package lesson0.eolymp;
 
 import java.util.Scanner;
 
-public class PrPr9920 {
+public class Pr9920 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         String s = in.next();
