05334cef04587113447da394abe0e0f7ab9e30e4
rovshan.huseynov <rovsen.huseyn.oglu@gmail.com>
2023-01-21 22:41:14 +0400

    -- solve eolymp problem 9914

---
+++ src/main/java/lesson0/eolymp/Pr9914.java
@@ -0,0 +1,10 @@
+package lesson0.eolymp;
+
+import java.util.Scanner;
+
+public class Pr9914 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Welcome, " + in.next() + "!");
+    }
+}
