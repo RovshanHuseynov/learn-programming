31ec2365a71ba469a9d70a145cfb9430ebb7ccb3
rovshan.huseynov <rovsen.huseyn.oglu@gmail.com>
2023-01-21 23:06:37 +0400

    -- solve eolymp problem 9921

---
+++ src/main/java/lesson0/eolymp/Pr9921.java
@@ -0,0 +1,14 @@
+package lesson0.eolymp;
+
+import java.util.Scanner;
+
+public class Pr9921 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String s = in.next();
+        System.out.println("- " + s);
+        System.out.println("- " + s);
+        System.out.println("- " + s);
+        System.out.println("- " + s);
+    }
+}
