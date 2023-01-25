c57db739f6ae8adfefe6e370ba8bbd24f4e4a5be
rovshan.huseynov <rovsen.huseyn.oglu@gmail.com>
2023-01-26 01:12:41 +0400

    -- solve eolymp problem 9934

--- src/main/java/lesson0/eolymp/Pr9934.java
+++ src/main/java/lesson0/eolymp/Pr9934.java
@@ -5,11 +5,10 @@ import java.util.Scanner;
 public class Pr9934 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int s = in.nextInt();
-        int h = s / 3600;
-        int m = (s - h * 3600) / 60;
-        int sec = s - h * 3600 - m * 60;
-
-        System.out.println(h + " " + m + " " + sec);
+        int m = in.nextInt();
+        int day = m / 1440;
+        int hour = (m - day * 1440) / 60;
+        int minute = m - day * 1440 - hour * 60;
+        System.out.println(++day + " " + hour + " " + minute);
     }
 }
