4085789fdcf442c51339f633aa2c72968926c890
rovshan.huseynov <rovsen.huseyn.oglu@gmail.com>
2023-01-25 19:28:19 +0400

    -- solve eolymp problem 9908

---
+++ src/main/java/lesson0/eolymp/Pr9908.java
@@ -0,0 +1,24 @@
+package lesson0.eolymp;
+
+import java.util.Scanner;
+
+public class Pr9908 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        long a = in.nextLong();
+        long b = in.nextLong();
+        long temp;
+
+        if(a > b) {
+            temp = a;
+            a = b;
+            b = temp;
+        }
+
+        int cnt=0;
+        for(long i=a; i<=b; i++){
+            if(i%2==0) cnt++;
+        }
+        System.out.println(cnt);
+    }
+}
