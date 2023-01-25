dce296608ce4bd6883a230bc48323479b172ca45
rovshan.huseynov <rovsen.huseyn.oglu@gmail.com>
2023-01-26 00:56:21 +0400

    -- solve eolymp problem 9931

--- src/main/java/lesson0/eolymp/Pr9931.java
+++ src/main/java/lesson0/eolymp/Pr9931.java
@@ -5,9 +5,8 @@ import java.util.Scanner;
 public class Pr9931 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int k = in.nextInt();
-        int ans = k/1024;
-        if(k % 1024 != 0) ans++;
-        System.out.println(ans);
+        int m = in.nextInt();
+        int h = m / 60;
+        System.out.println(h + " " + (m-h*60));
     }
 }
