22a896a8652c228d4cd5d55731ba42a031ca1927
rovshan.huseynov <rovsen.huseyn.oglu@gmail.com>
2023-01-26 23:50:26 +0400

    -- solve eolymp problem 5105

---
+++ src/main/java/lesson0/eolymp/Pr5105.java
@@ -0,0 +1,13 @@
+package lesson0.eolymp;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Pr5105 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        BigInteger a = in.nextBigInteger();
+        BigInteger b = in.nextBigInteger();
+        System.out.println(a.add(b));
+    }
+}
