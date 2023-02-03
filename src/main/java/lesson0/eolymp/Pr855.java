d35096a88170061a3a45d886a41b85152aa28118
rovshan.huseynov <rovsen.huseyn.oglu@gmail.com>
2023-02-03 14:12:25 +0400

    -- solve eolymp problem 855

--- src/main/java/lesson0/eolymp/Pr855.java
+++ src/main/java/lesson0/eolymp/Pr855.java
@@ -6,36 +6,39 @@ import java.util.Stack;
 public class Pr855 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+
         int n = in.nextInt();
-        //String text = in.nextLine();
-        if(n%2==0)
-            print("", n);
+        if(n%2==0) print("", n);
     }
 
-    private static boolean isValid(String text){
+    // isValid 0 - not Valid and not possible to be Valid
+    // isValid 1 - not Valid, but possible to be Valid
+    // isValid 2 - Valid
+    private static int isValid(String text){
         Stack<Character> s = new Stack<>();
         for(int i=0; i< text.length(); i++){
             if(text.charAt(i) == '(' || text.charAt(i) == '[') s.push(text.charAt(i));
             else {
-                if(s.empty()) return false;
+                if(s.empty()) return 0;
                 char top = s.peek();
                 char cur = text.charAt(i);
-                if(cur == ')' && top != '(') return false;
-                else if(cur == ']' && top != '[') return false;
+                if(cur == ')' && top != '(') return 0;
+                else if(cur == ']' && top != '[') return 0;
                 else s.pop();
             }
         }
-        return s.empty() && !text.isEmpty();
+
+        if(s.empty()) return 2;
+        else return 1;
     }
 
     private static void print(String text, int target) {
-        //System.out.println(text + " " + text.length());
-        //if(!text.isEmpty() && !isValid(text)) return;
-
         if (text.length() == target){
-            if (isValid(text)) System.out.println(text);
+            if (isValid(text) == 2) {
+                System.out.println(text);
+            }
         }
-        else {
+        else if(isValid(text) != 0){
             print(text + '(', target);
             print(text + '[', target);
             print(text + ')', target);
