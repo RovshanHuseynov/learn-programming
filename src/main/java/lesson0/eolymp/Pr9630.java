e770cc7249175982842b389646794c19f9169c61
rovshan.huseynov <rovsen.huseyn.oglu@gmail.com>
2023-02-03 15:57:02 +0400

    -- solve eolymp problem 9030

---
+++ src/main/java/lesson0/eolymp/Pr9630.java
@@ -0,0 +1,13 @@
+package lesson0.eolymp;
+
+import java.util.Scanner;
+
+public class Pr9630 {
+    Scanner in = new Scanner(System.in);
+    int n = in.nextInt();
+    int m = in.nextInt();
+
+    for(int i=0; i<m; i++){
+
+    }
+}
