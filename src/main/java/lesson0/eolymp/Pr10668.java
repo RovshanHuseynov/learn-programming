13f65ff875b285dfb2bc27c7eadc5455e10efd2e
rovshan.huseynov <rovsen.huseyn.oglu@gmail.com>
2023-02-04 22:17:52 +0400

    -- solve eolymp problem 10668

--- src/main/java/lesson0/eolymp/Pr10668.java
+++ src/main/java/lesson0/eolymp/Pr10668.java
@@ -1,5 +1,6 @@
 package lesson0.eolymp;
 
+import java.math.BigInteger;
 import java.util.Scanner;
 
 public class Pr10668 {
@@ -7,13 +8,13 @@ public class Pr10668 {
         Scanner in = new Scanner(System.in);
         long n = in.nextLong();
         long m = in.nextLong();
-        System.out.println(fac(n+m)/fac(n)/fac(m));
+        System.out.println(fac(n+m).divide(fac(n)).divide(fac(m)));
     }
 
-    private static long fac(long n){
-        long ans = 1L;
+    private static BigInteger fac(long n){
+        BigInteger ans = BigInteger.ONE;
         for(long i=2; i<=n; i++){
-            ans *= i;
+            ans = ans.multiply(BigInteger.valueOf(i));
         }
         return ans;
     }
