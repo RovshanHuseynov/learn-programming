fb2135556d40a58604d0018db9258006ef535c2a
rovshan.huseynov <rovsen.huseyn.oglu@gmail.com>
2023-02-24 15:37:33 +0400

    -- solve eolymp problem 977

--- src/main/java/lesson0/eolymp/Pr977.java
+++ src/main/java/lesson0/eolymp/Pr977.java
@@ -5,12 +5,12 @@ import java.util.Scanner;
 public class Pr977 {
     static int n;
     static int [][] a;
-    static boolean [] used;
+    static int [] used;
 
     private static void dfs(int from){
-        used[from] = true;
-        for(int to=0; to<n; to++){
-            if(!used[to] && a[from][to] == 1){
+        used[from] = 1;
+        for(int to=1; to<=n; to++){
+            if(used[to] == 0 && a[from][to] == 1){
                 dfs(to);
             }
         }
@@ -19,24 +19,24 @@ public class Pr977 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         n = in.nextInt();
-        a = new int[n][n];
-        used = new boolean[n];
+        a = new int[n+1][n+1];
+        used = new int[n+1];
 
         int cnt=0;
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n; j++){
                 a[i][j] = in.nextInt();
                 cnt = cnt + a[i][j];
             }
         }
 
-        if(cnt % 2 == 1 ||  n - 1 != cnt / 2) System.out.println("NO");
+        if(n - 1 != cnt / 2) System.out.println("NO");
         else {
-            dfs(0);
+            dfs(1);
 
             boolean ans = false;
-            for(int i=0; i<n; i++){
-                if(!used[i]){
+            for(int i=1; i<=n; i++){
+                if(used[i] == 0){
                     System.out.println("NO");
                     ans = true;
                 }
