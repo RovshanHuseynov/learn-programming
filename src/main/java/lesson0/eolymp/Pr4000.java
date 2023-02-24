65642f80b35854d2f5773611d2755337ebb5b148
rovshan.huseynov <rovsen.huseyn.oglu@gmail.com>
2023-02-24 17:07:14 +0400

    -- solve eolymp problem 4000

--- src/main/java/lesson0/eolymp/Pr4000.java
+++ src/main/java/lesson0/eolymp/Pr4000.java
@@ -3,17 +3,34 @@ package lesson0.eolymp;
 import java.util.Scanner;
 
 public class Pr4000 {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        int start = in.nextInt();
-        int x, ans = 1;
-        for(int i=1; i<=n; i++){
-            for(int j=1; j<=n; j++){
-                x = in.nextInt();
-                if(i == start) ans += x;
+    static int n;
+    static int start;
+    static int cnt;
+    static int [][] a;
+    static boolean [] used;
+
+    private static void dfs(int from){
+        used[from] = true;
+        for(int to=1; to<=n; to++){
+            if(!used[to] && a[from][to] == 1){
+                dfs(to);
+                cnt++;
             }
         }
-        System.out.println(ans);
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        n = in.nextInt();
+        a = new int[n+1][n+1];
+        used = new boolean[n+1];
+        start = in.nextInt();
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n; j++){
+                a[i][j] = in.nextInt();
+            }
+        }
+        dfs(start);
+        System.out.println(cnt+1);
     }
 }
