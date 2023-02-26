c6c2ebf6f71630589d62e6326967e63d4e7505ab
rovshan.huseynov <rovsen.huseyn.oglu@gmail.com>
2023-02-26 23:13:15 +0400

    -- solve eolymp problem 2270

--- src/main/java/lesson0/eolymp/Pr2270.java
+++ src/main/java/lesson0/eolymp/Pr2270.java
@@ -5,11 +5,24 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Pr2270 {
+    static boolean [] used;
+    static int n;
+    static List<List<Integer>> a;
+    private static void dfs(int from){
+        used[from] = true;
+        int len = a.get(from).size(), to;
+        for(int i=0; i<len; i++){
+            to = a.get(from).get(i);
+            if(!used[to]){
+                dfs(to);
+            }
+        }
+    }
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
+        n = in.nextInt();
         int m = in.nextInt();
-        List<List<Integer>> a = new ArrayList<>(n+1);
+        a = new ArrayList<>(n+1);
         for(int i=0; i<=n; i++){
             a.add(new ArrayList<>());
         }
@@ -19,5 +32,6 @@ public class Pr2270 {
             y = in.nextInt();
             a.get(x).add(y);
         }
+        dfs(1);
     }
 }
