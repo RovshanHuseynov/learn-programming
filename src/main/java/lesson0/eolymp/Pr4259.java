ccd65d8a39afb3b713651c1b05aae2a68968b0fc
rovshan.huseynov <rovsen.huseyn.oglu@gmail.com>
2023-03-21 23:51:15 +0400

    -- solve eolymp problem 4259

--- src/main/java/lesson0/eolymp/Pr4259.java
+++ src/main/java/lesson0/eolymp/Pr4259.java
@@ -10,7 +10,7 @@ public class Pr4259 {
         Stack<Integer> s = new Stack<>();
         int n = in.nextInt();
         int min = Integer.MAX_VALUE;
-        int t, p;
+        int t, p, top;
         for(int i=0; i<n; i++){
             t = in.nextInt();
             if(t == 1){
@@ -19,8 +19,12 @@ public class Pr4259 {
                 if(p < min) min = p;
             }
             else if (t == 2) {
+                top = s.peek();
                 s.pop();
-                min = s.stream().min(Comparator.comparingInt(o -> o)).orElse(0);
+                //System.out.print("print ");
+                //s.stream().forEach(val -> System.out.print(val + " "));
+                //System.out.println();
+                if(top == min) min = s.stream().min(Comparator.comparingInt(o -> o)).orElse(0);
             }
             else {
                 System.out.println(min);
