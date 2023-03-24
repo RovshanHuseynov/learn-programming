fb91073e07e8f82a093da2fb32cfb3b91c0d4c6e
rovshan.huseynov <rovsen.huseyn.oglu@gmail.com>
2023-03-24 13:00:44 +0400

    -- solve eolymp problem 4259

--- src/main/java/lesson0/eolymp/Pr4259.java
+++ src/main/java/lesson0/eolymp/Pr4259.java
@@ -5,31 +5,25 @@ import java.util.Stack;
 
 public class Pr4259 {
     public static void main(String[] args) {
+        // solution video: https://www.youtube.com/watch?v=ufwPuyxkNVE
         Scanner in = new Scanner(System.in);
-        Stack<Integer> s = new Stack<>();
+        Stack<Integer> m = new Stack<>(); // main stack
+        Stack<Integer> s = new Stack<>(); // supportion stack
         int n = in.nextInt();
-        int min = Integer.MAX_VALUE;
-        int t, p, top;
-        boolean flag = false;
+        int t, val, top;
         for(int i=0; i<n; i++){
             t = in.nextInt();
             if(t == 1){
-                p = in.nextInt();
-                s.push(p);
-
-                min = Math.min(p, min);
+               val = in.nextInt();
+               m.push(val);
+               if(s.size() == 0 || val < s.peek()) s.push(val);
             }
             else if (t == 2) {
-                top = s.peek();
-                s.pop();
-                //System.out.print("print ");
-                //s.stream().forEach(val -> System.out.print(val + " "));
-                //System.out.println();
-                if(top == min) flag = true;
+                top = m.pop();
+                if(!s.empty() && top == s.peek()) s.pop();
             }
             else {
-                if(flag) min = s.stream().mapToInt(v -> v).min().orElse(min);
-                System.out.println(min);
+                System.out.println(s.peek());
             }
         }
     }
