6356431b8db76bcc4f9dc1ece2596e7f0abd6687
rovshan.huseynov <rovsen.huseyn.oglu@gmail.com>
2023-03-26 00:56:00 +0400

    -- solve eolymp problem 9653

--- src/main/java/lesson0/eolymp/Pr9653.java
+++ src/main/java/lesson0/eolymp/Pr9653.java
@@ -1,15 +1,46 @@
 package lesson0.eolymp;
 
-import java.math.BigDecimal;
-import java.math.RoundingMode;
 import java.util.Scanner;
 
 public class Pr9653 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        double a = Double.parseDouble(in.nextLine());
-        double b = Double.parseDouble(in.nextLine());
-        double ans  = (a * b + 10) / 3 + (a + 2 / b - 7) * 5 - a - b / a;
-        System.out.println(BigDecimal.valueOf(ans).setScale(5, RoundingMode.HALF_UP).doubleValue());
+    }
+}
+
+class MyDouble {
+    private final double a;
+
+    MyDouble(double a) {
+        this.a = a;
+    }
+
+    public String toString() { // Print the variable of type MyLong
+        //return String.valueOf(BigDecimal.valueOf(a).setScale(5, RoundingMode.HALF_UP).doubleValue());
+        return String.valueOf(a);
+    }
+    public MyDouble Add(MyDouble b) {// Add the value of type MyDouble
+        return new MyDouble(a + b.a);
+    }
+    public MyDouble Add(double b) {// Add the value of type double
+        return Add(new MyDouble(b));
+    }
+    public MyDouble Sub(MyDouble b) {// Subtract the value of type MyDouble
+        return new MyDouble(a - b.a);
+    }
+    public MyDouble Sub(double b) { // Subtract the value of type double
+        return Sub(new MyDouble(b));
+    }
+    public MyDouble Mult(MyDouble b) {// Multiply by the value of type MyDouble
+        return new MyDouble(a * b.a);
+    }
+    public MyDouble Mult(double b) { // Multiply by the value of type double
+        return Mult(new MyDouble(b));
+    }
+    public MyDouble Div(MyDouble b) { // Divide by the value of type MyDouble
+        return new MyDouble(a / b.a);
+    }
+    public MyDouble Div(double b) { // Divide by the value of type double
+        return Div(new MyDouble(b));
     }
 }
