a7b795d2993537243add0e23cec2bde8405172cf
rovshan.huseynov <rovsen.huseyn.oglu@gmail.com>
2023-03-26 00:39:18 +0400

    -- solve eolymp problem 9652

--- src/main/java/lesson0/eolymp/Pr9652.java
+++ src/main/java/lesson0/eolymp/Pr9652.java
@@ -1,13 +1,29 @@
 package lesson0.eolymp;
 
-import java.math.BigInteger;
 import java.util.Scanner;
 
 public class Pr9652 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        BigInteger a = in.nextBigInteger();
-        BigInteger b = in.nextBigInteger();
-        System.out.println(a.add(b).add(new BigInteger("10")));
+        MyLong myLong = new MyLong(in.nextLong());
+        myLong.Add(in.nextLong());
+        System.out.println(myLong.toString());
+    }
+}
+
+class MyLong
+{
+    private long a;
+    MyLong(long a) {
+        this.a = a;
+    }
+    public String toString() {
+        return String.valueOf(a);
+    }
+    public MyLong Add(MyLong b) {
+        return Add(b.a);
+    }
+    public MyLong Add(long b) {
+        return new MyLong(a + b);
     }
 }
