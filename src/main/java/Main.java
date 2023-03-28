fad5b5de24ab433a6f91c4bc7ff13d5e48aa0422
rovshan.huseynov <rovsen.huseyn.oglu@gmail.com>
2023-03-28 23:27:10 +0400

    -- trivial changes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,8 +4,10 @@ public class Main {
         int a = 04;
         int b = 011;
         int c = 0100;
+        int d = 0x100;
         System.out.println(a);
         System.out.println(b);
         System.out.println(c);
+        System.out.println(d);
     }
 }
