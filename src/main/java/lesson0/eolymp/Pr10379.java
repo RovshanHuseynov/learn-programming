977c4133004d6ccd69431e0349e9118b6d5e0f3e
rovshan.huseynov <rovsen.huseyn.oglu@gmail.com>
2023-03-31 02:03:33 +0400

    -- solve eolymp problem Pr10379

--- src/main/java/lesson0/eolymp/Pr10379.java
+++ src/main/java/lesson0/eolymp/Pr10379.java
@@ -1,28 +1,45 @@
 package lesson0.eolymp;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 public class Pr10379 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        String s;
-        int val;
-        Map<Integer, Integer> mapFreq = new HashMap<>();
+        String command;
+        int curObject, cntCurObject, max = 0, sizeOfMax, poppedObject;
+        Map<Integer, Integer> mapCntObject = new HashMap<>();
+        Map<Integer, List<Integer>> listAccordingToCount = new HashMap<>();
         while(in.hasNext()){
-            s = in.next();
-            if(s.equals("push")){
-                val = in.nextInt();
-                if(mapFreq.containsKey(val)) {
-                    mapFreq.put(val, mapFreq.get(val) + 1);
-                } else {
-                    mapFreq.put(val, 1);
-                }
-                // push
+            command = in.next();
+            if(command.equals("push")){
+                curObject = in.nextInt();
+                cntCurObject = mapCntObject.getOrDefault(curObject, 0) + 1;
+                mapCntObject.put(curObject, cntCurObject);
+                max = Math.max(max, cntCurObject);
+                List<Integer> integers = listAccordingToCount.getOrDefault(cntCurObject, new ArrayList<>());
+                integers.add(curObject);
+                listAccordingToCount.put(cntCurObject, integers);
             } else {
-                // pop
+                List<Integer> maxCntObjects = listAccordingToCount.get(max);
+                sizeOfMax = maxCntObjects.size();
+                poppedObject = maxCntObjects.get(maxCntObjects.size()-1);
+                mapCntObject.put(poppedObject, mapCntObject.get(poppedObject)-1);
+                listAccordingToCount.get(max).remove((Object)poppedObject);
+                if(sizeOfMax == 1) max--;
+                System.out.println(poppedObject);
+                //System.out.println(mapCntObject);
+                //System.out.println(listAccordingToCount);
             }
         }
     }
 }
+/*
+push 5
+push 4
+push 5
+push 3
+push 4
+push 2
+push 5
+pop
+*/
