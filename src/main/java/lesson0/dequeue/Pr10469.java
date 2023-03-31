554c3c19a2bb77a58354c78888bbc0bd85f3eac8
rovshan.huseynov <rovsen.huseyn.oglu@gmail.com>
2023-04-01 02:16:44 +0400

    -- solve eolymp problem 10469

--- src/main/java/lesson0/dequeue/Pr10469.java
+++ src/main/java/lesson0/dequeue/Pr10469.java
@@ -4,34 +4,34 @@ import java.util.ArrayDeque;
 import java.util.Deque;
 import java.util.Scanner;
 
-public class Pr10468 {
+public class Pr10469 {
     public static void main(String[] args) {
         Deque<Integer> deque = new ArrayDeque<>();
         Scanner in = new Scanner(System.in);
         String command1, command2;
-        int val, sum = 0;
+        int val, cnt = 0;
         while(in.hasNext()){
             command1 = in.next();
             command2 = in.next();
             if(command1.equals("push") && command2.equals("front")){
                 val = in.nextInt();
-                if(val % 2 == 1) sum += val;
+                if(val % 2 == 0) cnt++;
                 deque.addFirst(val);
             } else if(command1.equals("push") && command2.equals("back")){
                 val = in.nextInt();
-                if(val % 2 == 1) sum += val;
+                if(val % 2 == 0) cnt++;
                 deque.addLast(val);
             } else if(command1.equals("pop") && command2.equals("front")){
                 val = deque.getFirst();
-                if(val % 2 == 1) sum -= val;
+                if(val % 2 == 0) cnt--;
                 deque.removeFirst();
             } else if(command1.equals("pop") && command2.equals("back")){
                 val = deque.getLast();
-                if(val % 2 == 1) sum -= val;
+                if(val % 2 == 0) cnt--;
                 deque.removeLast();
             }
         }
-        System.out.println(sum);
+        System.out.println(cnt);
         // Ctrl + D to stop
     }
 }
