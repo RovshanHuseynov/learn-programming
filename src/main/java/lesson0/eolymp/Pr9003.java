d87be0ce2e5368a6e6b79dedb85e190d64ed1ec1
rovshan.huseynov <rovsen.huseyn.oglu@gmail.com>
2023-04-06 22:40:48 +0400

    -- solve eolymp problem 9003

--- src/main/java/lesson0/eolymp/Pr9003.java
+++ src/main/java/lesson0/eolymp/Pr9003.java
@@ -5,5 +5,19 @@ import java.util.Scanner;
 public class Pr9003 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int [][] a = new int[201][201];
+        for(int i=1; i<=n; i++) {
+            for (int j = 1; j <= n; j++) {
+                a[i][j] = in.nextInt();
+            }
+        }
+
+        for(int i=1; i<=n; i++) {
+            for (int j = 1; j <= n; j++) {
+                System.out.print(a[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
