0c7458fd1fb12fb9fc49b20b1927f75920f1fd0f
rovshan.huseynov <rovsen.huseyn.oglu@gmail.com>
2023-04-09 17:07:58 +0400

    -- solve eolymp problem 10082

--- src/main/java/lesson0/eolymp/Pr10082.java
+++ src/main/java/lesson0/eolymp/Pr10082.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public class Pr10082 {
     static int n, m, s, d;
-    static boolean [] used;
+    //static boolean [] used;
     static int [] dis;
     static List<List<Integer>> a;
 
@@ -14,45 +14,48 @@ public class Pr10082 {
         m = in.nextInt();
         s = in.nextInt();
         d = in.nextInt();
-
-        a = new ArrayList<>(n+1);
-        used = new boolean[n+1];
+        n = 2 * n;
+        //used = new boolean[n+1];
         dis = new int[n+1];
+        a = new ArrayList<>(n+1);
         for(int i=0; i<=n; i++){
             a.add(new ArrayList<>());
+            dis[i] = -1;
         }
 
         int u,v;
         for(int i=1; i<=m; i++){
             u = in.nextInt();
             v = in.nextInt();
-            a.get(u).add(v);
-            a.get(v).add(u);
+            a.get(2*u).add(2*v-1);
+            a.get(2*v-1).add(2*u);
+            a.get(2*v).add(2*u-1);
+            a.get(2*u-1).add(2*v);
         }
-        bsf();
+
+        bsf(2*s-1);
         /*for(int i=1; i<=n; i++){
             System.out.print(dis[i] + " ");
         }
         System.out.println();*/
-        if(dis[d] == 0 || dis[d] %2 == 1) dis[d] = -1;
-        System.out.println(dis[d]);
+
+        System.out.println(dis[2*d-1]);
     }
 
-    static void bsf(){
+    static void bsf(int start){
         Queue<Integer> queue = new LinkedList<>();
-        queue.add(s);
-        used[s] = true;
-        dis[s] = 0;
-        int from, to, len;
+        queue.add(start);
+        dis[start] = 0;
+        int from, to,len;
 
         while(!queue.isEmpty()){
             from = queue.poll();
             len = a.get(from).size();
+
             for(int i=0; i<len; i++){
                 to = a.get(from).get(i);
-                if(!used[to]){
-                    used[to] = true;
-                    dis[to] = dis[from]+1;
+                if(dis[to] == -1){
+                    dis[to] = dis[from] + 1;
                     queue.add(to);
                 }
             }
