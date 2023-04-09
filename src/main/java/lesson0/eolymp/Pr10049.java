303b612eb902843596add3dae2e124b668c4b741
rovshan.huseynov <rovsen.huseyn.oglu@gmail.com>
2023-04-09 22:27:38 +0400

    -- solve eolymp problem 10049

---
+++ src/main/java/lesson0/eolymp/Pr10049.java
@@ -0,0 +1,17 @@
+package lesson0.eolymp;
+
+import java.util.Scanner;
+
+public class Pr10049 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        int n = in.nextInt();
+        int m = in.nextInt();
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=m; j++){
+
+            }
+        }
+    }
+}
