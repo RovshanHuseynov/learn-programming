ff2fee9588af504343e36c3435b4e5dc41a3eef0
rovshan.huseynov <rovsen.huseyn.oglu@gmail.com>
2023-04-10 15:15:27 +0400

    -- solve eolymp problem 8234

--- src/main/java/lesson0/eolymp/Pr8234.java
+++ src/main/java/lesson0/eolymp/Pr8234.java
@@ -6,12 +6,13 @@ public class Pr8234 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
-        long [] dp = new long[n+1];
+        long [] dp = new long[61];
         dp[0]=1;
-        dp[1]=2;
+        dp[1]=1;
+        dp[2]=2;
 
-        for(int i=2; i<=n; i++){
-            dp[i] = dp[i-2]*2;
+        for(int i=3; i<=n; i++){
+            dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
         }
         System.out.println(dp[n]);
     }
