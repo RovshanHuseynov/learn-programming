348fc42e48c70c5ff94828cb6b7a80e3a235f29c
rovshan.huseynov <rovsen.huseyn.oglu@gmail.com>
2023-04-11 01:23:57 +0400

    -- solve eolymp problem 11407

---
+++ src/main/java/lesson0/eolymp/Pr11407.java
@@ -0,0 +1,23 @@
+package lesson0.eolymp;
+
+import java.util.Scanner;
+
+public class Pr11407 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int space = 0;
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=space; j++){
+                System.out.print(" ");
+            }
+            System.out.print("@");
+            for(int j=1; j<=(2*n-1-2-2*space); j++){
+                System.out.print("*");
+            }
+            if(i!=n)System.out.print("@");
+            System.out.println();
+            space++;
+        }
+    }
+}
