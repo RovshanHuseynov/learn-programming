88285e68898cc09c6f7a1037859434ea69e0eecf
rovshan.huseynov <rovsen.huseyn.oglu@gmail.com>
2023-04-11 01:54:44 +0400

    -- solve eolymp problem 11412

---
+++ src/main/java/lesson0/eolymp/Pr11412.java
@@ -0,0 +1,24 @@
+package lesson0.eolymp;
+
+import java.util.Scanner;
+
+public class Pr11412 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n; j++){
+                if(i+j==n+1) System.out.print("<");
+                else System.out.print(" ");
+            }
+            System.out.println();
+        }
+        for(int i=n+1; i<=2*n-1; i++){
+            for(int j=1; j<=n; j++){
+                if(i-j==n-1) System.out.print("<");
+                else System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
