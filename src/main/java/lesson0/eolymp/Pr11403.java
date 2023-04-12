2de4897de1ed75d95ea4c720966d653c10293900
rovshan.huseynov <rovsen.huseyn.oglu@gmail.com>
2023-04-12 17:32:40 +0400

    -- solve eolymp problem 11403

---
+++ src/main/java/lesson0/eolymp/Pr11403.java
@@ -0,0 +1,12 @@
+package lesson0.eolymp;
+
+import java.util.Scanner;
+
+public class Pr11403 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int m = in.nextInt();
+        System.out.println(m*(m+1)/2);
+    }
+}
