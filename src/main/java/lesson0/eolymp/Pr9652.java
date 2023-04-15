ac70689bb151eedffe9f79e1943616961f7543a6
rovshan.huseynov <rovsen.huseyn.oglu@gmail.com>
2023-04-15 15:32:51 +0400

    -- solve eolymp problem 9652

--- src/main/java/lesson0/eolymp/Pr9652.java
+++ src/main/java/lesson0/eolymp/Pr9652.java
@@ -5,25 +5,25 @@ import java.util.Scanner;
 public class Pr9652 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        MyLong myLong = new MyLong(in.nextLong());
+        MyLong9652 myLong = new MyLong9652(in.nextLong());
         myLong.Add(in.nextLong());
         System.out.println(myLong);
     }
 }
 
-class MyLong
+class MyLong9652
 {
     private final long a;
-    MyLong(long a) {
+    MyLong9652(long a) {
         this.a = a;
     }
     public String toString() {
         return String.valueOf(a);
     }
-    public MyLong Add(MyLong b) {
+    public MyLong9652 Add(MyLong9652 b) {
         return Add(b.a);
     }
-    public MyLong Add(long b) {
-        return new MyLong(a + b);
+    public MyLong9652 Add(long b) {
+        return new MyLong9652(a + b);
     }
 }
