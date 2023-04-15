2f959c5b2894d721998c543c891f0cd61d3b1948
rovshan.huseynov <rovsen.huseyn.oglu@gmail.com>
2023-04-15 14:57:08 +0400

    -- solve eolymp problem 9646

--- src/main/java/lesson0/eolymp/Pr9646.java
+++ src/main/java/lesson0/eolymp/Pr9646.java
@@ -1,16 +1,27 @@
 package lesson0.eolymp;
 
+import java.math.BigInteger;
 import java.util.Scanner;
 
 public class Pr9646 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        long n = in.nextLong();
-        long m = in.nextLong();
-        long ans = 0;
+        int n = in.nextInt();
+        int m = in.nextInt();
+        BigInteger ans = BigInteger.ZERO;
         for(int i=1; i<=n; i++){
-            ans += Math.pow(i,i);
+            ans = ans.add(pow(BigInteger.valueOf(i),BigInteger.valueOf(i)));
         }
-        System.out.println(ans%m);
+        System.out.println(ans.mod(BigInteger.valueOf(m)));
+    }
+
+    static BigInteger pow(BigInteger n, BigInteger power){
+        if(power.equals(BigInteger.ONE)) return n;
+
+        if(power.mod(BigInteger.TWO).equals(BigInteger.ZERO)){
+            return pow(n, power.divide(BigInteger.TWO)).multiply(pow(n, power.divide(BigInteger.TWO)));
+        } else {
+            return pow(n, power.divide(BigInteger.TWO)).multiply(pow(n, power.divide(BigInteger.TWO))).multiply(n);
+        }
     }
 }
