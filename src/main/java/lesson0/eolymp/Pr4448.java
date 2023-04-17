636f06e96a11cfebcc6dddad54f1ee6e7cdfa6ae
rovshan.huseynov <rovsen.huseyn.oglu@gmail.com>
2023-04-17 23:23:05 +0400

    -- solve eolymp problem 4448

--- src/main/java/lesson0/eolymp/Pr4448.java
+++ src/main/java/lesson0/eolymp/Pr4448.java
@@ -5,6 +5,11 @@ import java.util.Scanner;
 public class Pr4448 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-
+        String king = in.nextLine();
+        String rook;
+        int n = in.nextInt();
+        for(int i=1; i<=n; i++){
+            rook = in.nextLine();
+        }
     }
 }
