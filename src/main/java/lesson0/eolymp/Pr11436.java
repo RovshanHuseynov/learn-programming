32300f5c7130dad78b43fbf04ebd2fead346d28c
rovshan.huseynov <rovsen.huseyn.oglu@gmail.com>
2023-04-18 23:41:59 +0400

    -- solve eolymp problem 11436

--- src/main/java/lesson0/eolymp/Pr11436.java
+++ src/main/java/lesson0/eolymp/Pr11436.java
@@ -5,18 +5,17 @@ import java.util.Scanner;
 public class Pr11436 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        String s =
-                //"!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
-                in.nextLine();
-        /*String template = ".,'?!*-_@()[]{}:;/\"";
+        String s = in.nextLine();
+        String template = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ ";
+                //in.nextLine();
+        //String template = ".,'?!*-_@()[]{}:;/\"\\";
         int cnt=0;
 
         System.out.println(s);
         for(int i=0; i<s.length(); i++){
-            System.out.println(s.charAt(i) + " " + template.contains(s.charAt(i) + ""));
+            //System.out.println(s.charAt(i) + " " + template.contains(s.charAt(i) + ""));
             if(template.contains(s.charAt(i) + "")) cnt++;
         }
-        System.out.println(cnt);*/
-        System.out.println(s.length());
+        System.out.println(cnt);
     }
 }
