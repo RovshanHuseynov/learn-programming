646fa0f7e7da03fc6d59c2ac77fef9dd7ad5ae94
rovshan.huseynov <rovsen.huseyn.oglu@gmail.com>
2023-04-20 02:41:07 +0400

    -- solve eolymp problem 11434

---
+++ src/main/java/lesson0/eolymp/Pr11434.java
@@ -0,0 +1,13 @@
+package lesson0.eolymp;
+
+import java.util.Scanner;
+
+public class Pr11434 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        float f = n+1;
+
+        System.out.printf("%.6f\n" , f);
+    }
+}
