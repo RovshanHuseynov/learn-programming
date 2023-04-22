0492a781ee4bca072cfb4de7a939aa29e38b0353
rovshan.huseynov <rovsen.huseyn.oglu@gmail.com>
2023-04-22 23:53:55 +0400

    -- solve eolymp problem 2631

--- src/main/java/lesson0/eolymp/Pr2631.java
+++ src/main/java/lesson0/eolymp/Pr2631.java
@@ -2,7 +2,9 @@ package lesson0.eolymp;
 
 import java.util.List;
 import java.util.Scanner;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class Pr2631 {
@@ -22,15 +24,24 @@ public class Pr2631 {
                         .map(Integer::parseInt)
                         .collect(Collectors.toList());
                 size = l.size();
-                // TODO write it with map and flatMap
                 for(int j=0; j<size-1; j++){
                     for(int k=j+1; k<size; k++){
                         max = Math.max(max, ebob(l.get(j), l.get(k)));
                     }
                 }
+
+                //List<Integer> finalL = l;
+                //l.forEach(j -> finalL.forEach(k -> System.out.println(j + "-" + k)));
+
+                AtomicInteger maxx = new AtomicInteger();
+                int finalSize = size;
+                List<Integer> finalL1 = l;
+                IntStream.range(0, size).forEach(j -> IntStream.range(j+1, finalSize).forEach(k -> maxx.set(Math.max(maxx.get(), ebob(finalL1.get(j), finalL1.get(k))))));
+
                 //System.out.println(size);
-                System.out.println(max);
+                //System.out.println(max);
                 //System.out.println();
+                System.out.println(maxx);
             }
         }
     }
