59f0a324875fc3ed0aaacf41aaa20035b7e28815
rovshan.huseynov <rovsen.huseyn.oglu@gmail.com>
2023-04-28 12:20:14 +0400

    -- solve eolymp problem 10050

--- src/main/java/lesson0/eolymp/Pr10050.java
+++ src/main/java/lesson0/eolymp/Pr10050.java
@@ -1,18 +1,73 @@
 package lesson0.eolymp;
 
-import java.util.Scanner;
+import java.util.*;
+import java.util.stream.IntStream;
 
 public class Pr10050 {
+    static List<List<Path10050>> a;
+    static int n;
+    static long [] dis;
+    static boolean [] used;
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        int a,b,x;
-        int [][] arr = new int[n+1][n+1];
-        for(int i=0; i<n; i++){
-            a = in.nextInt();
-            b = in.nextInt();
-            x = in.nextInt();
-            arr[a][b] = x;
+        n = in.nextInt();
+        a = new ArrayList<>(n+1);
+        int u,v,w;
+        for(int i=0; i<=n; i++){
+            a.add(new ArrayList<>());
         }
+        for(int i=1; i<n; i++){
+            u = in.nextInt();
+            v = in.nextInt();
+            w = in.nextInt();
+            a.get(u).add(new Path10050(v,w));
+            a.get(v).add(new Path10050(u,w));
+        }
+
+        bfs(1);
+        int maxIndex = IntStream.rangeClosed(1, n)
+                .reduce((i, j) -> dis[i] > dis[j] ? i : j)
+                .orElse(0);
+
+        bfs(maxIndex);
+        System.out.println(Arrays.stream(dis)
+                .max()
+                .orElse(0));
+    }
+
+    private static void bfs(int start){
+        dis = new long[n+1];
+        used = new boolean[n+1];
+
+        Queue<Integer> queue = new LinkedList<>();
+        used[start] = true;
+        queue.add(start);
+
+        while(!queue.isEmpty()){
+            int from = queue.poll();
+            int len = a.get(from).size();
+            int to;
+            long w;
+
+            for(int i=0; i<len; i++){
+                to = a.get(from).get(i).to;
+                w = a.get(from).get(i).w;
+
+                if(!used[to]){
+                    queue.add(to);
+                    used[to] = true;
+                    dis[to] = Math.max(dis[to], dis[from] + w);
+                }
+            }
+        }
+    }
+}
+
+class Path10050 {
+    int to;
+    long w;
+    public Path10050(int to, long w){
+        this.to = to;
+        this.w = w;
     }
 }
