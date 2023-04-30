cb42c13a99dfb9b4e692314bd45b5645278a70da
rovshan.huseynov <rovsen.huseyn.oglu@gmail.com>
2023-04-30 15:14:26 +0400

    -- implement segment tree

--- src/main/java/lesson0/SegmentTree.java
+++ src/main/java/lesson0/SegmentTree.java
@@ -8,30 +8,38 @@ public class SegmentTree {
     1 3 5 7 9 11
      */
     // https://www.hackerearth.com/practice/data-structures/advanced-data-structures/segment-trees/tutorial/
+
+    static int [] a;
+    static int [] tree;
+    static int n;
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        int [] arr = new int[n];
-        int [] segmentTree = new int[n*2+1];
+        n = in.nextInt();
+        a = new int[n];
+        tree = new int[2*n+2];
 
         for(int i=0; i<n; i++){
-            arr[i] = in.nextInt();
+            a[i] = in.nextInt();
+        }
+
+        build(1, 0, n-1);
+        for(int i=1; i<2*n; i++){
+            System.out.println(i + " " + tree[i]);
         }
     }
 
-    private static void build(){
+    private static void build(int node, int l, int r){
+        if(l > r || l < 0) return;
 
-    }
-
-    private static int build1(int[] arr, int left, int right){
-        if(left == right) {
-            //System.out.println(left + " " + right + " " + arr[left]);
-            return arr[left];
+        if(l == r){
+            tree[node] = a[l];
+            return;
         }
 
-        int mid = (left + right) / 2;
-        int ans = build1(arr,left, mid) + build1(arr, mid+1, right);
-        //System.out.println(left + " " + right + " " + ans);
-        return ans;
+        int mid = (l+r)/2;
+        build(2*node, l, mid);
+        build(2*node+1, mid+1,r);
+        tree[node] = tree[2*node] + tree[2*node+1];
     }
 }
