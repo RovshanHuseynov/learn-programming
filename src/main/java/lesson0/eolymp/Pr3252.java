523fd804fa8d6a7750d9b3aecc06699b52c23dbc
rovshan.huseynov <rovsen.huseyn.oglu@gmail.com>
2023-05-05 17:00:44 +0400

    -- solve eolymp problem 3252

--- src/main/java/lesson0/eolymp/Pr3252.java
+++ src/main/java/lesson0/eolymp/Pr3252.java
@@ -1,5 +1,6 @@
 package lesson0.eolymp;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Pr3252 {
@@ -12,27 +13,39 @@ public class Pr3252 {
         n = in.nextInt();
         a = new int[n];
         t = new int[2*n];
+        for(int i=0; i<n; i++){
+            a[i] = in.nextInt();
+        }
+        /*
+        8
+        9 2 5 4 8 6 1 8
+         */
         p = 91;
         q = 47;
         build(1, 0, n-1);
+        Arrays.stream(t).forEach(i -> System.out.print(i + " "));
+        System.out.println();
         int m = in.nextInt();
         char c;
         int p,q;
         for(int i=1; i<=m; i++){
-            c = in.next().charAt(0);
+            //c = in.next().charAt(0);
             p = in.nextInt();
             q = in.nextInt();
 
+            c = '?';
             if(c == '!'){
                 // update
             } else if(c == '?'){
                 // return sum
+                System.out.println(query(1,0,n-1,p,q));
             }
         }
     }
 
     private static void build(int node, int l, int r){
         if(l > r || r < 0) return;
+
         if(l == r){
             t[node] = a[l];
             return;
@@ -43,6 +56,13 @@ public class Pr3252 {
         build(2*node+1,mid+1,r);
         t[node] = t[2*node] + t[2*node+1];
     }
-    private static void query(int node, int l, int r){}
+    private static int query(int node, int l, int r, int start, int end){
+        if(start > r || end < l) return 0;
+
+        if(start <= l && end >= r) return t[node];
+
+        int mid = (l+r)/2;
+        return query(2*node, l, mid, start, end) + query(2*node+1, mid+1,r, start, end);
+    }
     private static void update(int node, int l, int r, int val){}
 }
