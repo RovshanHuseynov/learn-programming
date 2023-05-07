3e44ebe19f2eb356bdd39d984cea8067a6f15304
rovshan.huseynov <rovsen.huseyn.oglu@gmail.com>
2023-05-07 23:19:04 +0400

    -- solve eolymp problem 3984

---
+++ src/main/java/lesson0/eolymp/Pr3984.java
@@ -0,0 +1,10 @@
+package lesson0.eolymp;
+
+import java.util.Scanner;
+
+public class Pr3984 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+    }
+}
