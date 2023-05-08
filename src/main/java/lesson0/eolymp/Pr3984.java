54b8ae4538a8d7d6a11cb1535c2c14619490720c
rovshan.huseynov <rovsen.huseyn.oglu@gmail.com>
2023-05-08 22:54:46 +0400

    -- solve eolymp problem 3984

--- src/main/java/lesson0/eolymp/Pr3984.java
+++ src/main/java/lesson0/eolymp/Pr3984.java
@@ -5,6 +5,6 @@ import java.util.Scanner;
 public class Pr3984 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-
+        int n = in.nextInt();
     }
 }
