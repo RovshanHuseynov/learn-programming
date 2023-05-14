5faa4aea158444e450d84bc9b397173e34657cc0
rovshan.huseynov <rovsen.huseyn.oglu@gmail.com>
2023-05-14 23:43:18 +0400

    -- solve eolymp problem 5084

--- src/main/java/lesson0/eolymp/Pr5084.java
+++ src/main/java/lesson0/eolymp/Pr5084.java
@@ -8,17 +8,24 @@ public class Pr5084 {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
         int [] a = new int[n+1];
+        int [] b;
         for(int i=1; i<=n; i++){
             a[i] = in.nextInt();
         }
-        Arrays.sort(a);
         int q = in.nextInt();
         int l,r,x;
         for(int i=1; i<=q; i++){
             l = in.nextInt();
             r = in.nextInt();
             x = in.nextInt();
-            System.out.println(binarySearch(l,r,x,a));
+
+            b = new int[r-l+2];
+            int cnt=0;
+            for(int j=l; j<=r; j++){
+                b[++cnt] = a[j];
+            }
+            Arrays.sort(b);
+            System.out.println(binarySearch(1,(r-l+1),x,b));
         }
     }
 
