f6486b0a3e7f9c562d6e4fb6388b036d5931188c
rovshan.huseynov <rovsen.huseyn.oglu@gmail.com>
2023-05-15 02:25:34 +0400

    -- solve eolymp problem 5084_2

--- src/main/java/lesson0/eolymp/Pr5084_2.java
+++ src/main/java/lesson0/eolymp/Pr5084_2.java
@@ -9,7 +9,7 @@ import java.util.stream.Stream;
 public class Pr5084_2 {
     private static int n;
     private static int[] a;
-    private static List<Integer>[] t;
+    private static ArrayList[] t;
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         n = in.nextInt();
@@ -19,64 +19,65 @@ public class Pr5084_2 {
             a[i] = in.nextInt();
         }
         build(1,1,n);
-        for(int i=1; i<=4*n; i++){
-            //System.out.println(i + "->" + t[i]);
-        }
         int q = in.nextInt();
         int l,r,x;
         for(int i=1; i<=q; i++){
             l = in.nextInt();
             r = in.nextInt();
             x = in.nextInt();
+            System.out.println(query(1,1,n,x,l,r));
         }
     }
 
     private static void build(int node, int l, int r){
         if(l>r || r<1) return;
         if(l == r){
-            //System.out.println("stop " + node + " " + l + " " + a.get(l));
-            //System.out.println("stop " + node + " " + a.get(l));
-            //System.out.println(t.get(node));
             t[node] = new ArrayList<>();
             t[node].add(a[l]);
-            //System.out.println(t.get(node));
             return;
         }
 
         int mid = (l+r)/2;
         build(2*node,l,mid);
         build(2*node+1,mid+1,r);
-        List<Integer> combined = generate(node);
         if(t[node] == null) t[node] = new ArrayList<>();
-        t[node].addAll(combined);
+        t[node].addAll(mergeAndSort(node));
     }
 
-    private static List<Integer> generate(int node){
-        //System.out.println("generate " + node);
+    private static List<Integer> mergeAndSort(int node){
         List<Integer> l = t[2*node];
         List<Integer> r = t[2*node+1];
         List<Integer> ans = Stream.concat(l.stream(), r.stream())
                 .sorted()
                 .collect(Collectors.toList());
-        //System.out.println("node " + node);
-        //System.out.println(l);
-        //System.out.println(r);
-        //System.out.println(node + " ans->" + ans);
         return ans;
     }
 
-    private static int binarySearch(int l, int r, int val, int[] a){
-        //System.out.println(l + " " + r + " " + val);
-        if(r<1 || l>r || l>a.length || a[l]>=val) {
-            //System.out.println(l + " " + r + " stop");
-            return 0;
-        }
-        if(a[r]<val) {
-            //System.out.println(l + " " + r + " result " + (r-l+1));
-            return r-l+1;
+    private static int query(int node, int l, int r, int val, int start, int end){
+        if(l > end || r < start) return 0;
+        if(l >= start && r <= end) {
+            return binarySearch(1,t[node].size(),val,convertListToArray(t[node]));
         }
 
         int mid = (l+r)/2;
+        return query(2*node,l,mid,val,start,end)+query(2*node+1,mid+1,r,val,start,end);
+    }
+
+    private static int binarySearch(int l, int r, int val, int[] a){
+        if(r<1 || l>r || l>a.length || a[l]>=val) return 0;
+        if(a[r]<val) return r-l+1;
+
+        int mid = (l+r)/2;
         return binarySearch(l,mid,val,a) + binarySearch(mid+1,r,val,a);
+    }
+
+    private static int [] convertListToArray(List<Integer> l){
+        int len = l.size();
+        int [] a = new int[len+1];
+        a[0] = 0;
+        for(int i=0; i<len; i++){
+            a[i+1] = l.get(i);
+        }
+        return a;
     }
 }
