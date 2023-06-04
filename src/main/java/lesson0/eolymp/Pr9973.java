0c84049ecc4bdc4b34c5f4b05311b0a92d9bf19f
rovshan.huseynov <rovsen.huseyn.oglu@gmail.com>
2023-06-04 23:28:01 +0400

    -- solve eolymp problem 9973

---
+++ src/main/java/lesson0/eolymp/Pr9973.java
@@ -0,0 +1,11 @@
+package lesson0.eolymp;
+
+import java.util.Scanner;
+
+public class Pr9973 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        System.out.println(n*n*(n+1)*(n+1)/4);
+    }
+}
