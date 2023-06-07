e69f82997a04b65b31ff87782247def66e437654
rovshan.huseynov <rovsen.huseyn.oglu@gmail.com>
2023-06-07 18:12:40 +0400

    -- solve eolymp problem 9978

--- src/main/java/lesson0/eolymp/Pr9976.java
+++ src/main/java/lesson0/eolymp/Pr9976.java
@@ -7,8 +7,12 @@ public class Pr9976 {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
         double sum = 0.0;
+        int a = 1;
+        int b = 1;
         for(int i=1; i<=n; i++){
-            sum += 1.0*i/(i+1);
+            sum += 1.0*a/b;
+            a++;
+            b+=2;
         }
         System.out.printf("%.3f\n",sum);
     }
