44bc5fd283c0dfca39f2727ef201ee16ac159384
rovshan.huseynov <rovsen.huseyn.oglu@gmail.com>
2023-06-09 14:38:35 +0400

    -- solve eolymp problem 6958

--- src/main/java/lesson0/eolymp/Pr6958.java
+++ src/main/java/lesson0/eolymp/Pr6958.java
@@ -1,7 +1,18 @@
 package lesson0.eolymp;
 
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class Pr6958 {
     public static void main(String[] args) {
-        System.out.println("153 370 371 407");
+        Scanner in = new Scanner(System.in);
+        String text;
+        while(in.hasNext()){
+            text = in.nextLine();
+            String pattern1 = "da+dd?(i|y)";
+            Pattern pattern = Pattern.compile(pattern1);
+            if(pattern.matcher(text).matches()) System.out.println("She called me!!!");
+            else System.out.println("Cooing");
+        }
     }
 }
