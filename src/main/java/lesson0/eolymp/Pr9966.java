6342c4331e0668b6036d1a52f1622ceeee43f3e6
rovshan.huseynov <rovsen.huseyn.oglu@gmail.com>
2023-06-12 15:42:59 +0400

    -- solve eolymp problem 9966

---
+++ src/main/java/lesson0/eolymp/Pr9966.java
@@ -0,0 +1,17 @@
+package lesson0.eolymp;
+
+import java.util.Scanner;
+
+public class Pr9966 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        long last = 4L;
+        long sum = 2L;
+        for(int i=1; i<=n; i++){
+            System.out.print(sum + " ");
+            sum += last;
+            last += 2L;
+        }
+    }
+}
