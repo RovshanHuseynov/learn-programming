4419afa1cab99a93c6ab158a65a6f9f9e111b2be
rovshan.huseynov <rovsen.huseyn.oglu@gmail.com>
2023-06-12 15:44:15 +0400

    -- solve eolymp problem 9966

--- src/main/java/lesson0/eolymp/Pr9966.java
+++ src/main/java/lesson0/eolymp/Pr9966.java
@@ -6,12 +6,8 @@ public class Pr9966 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
-        long last = 4L;
-        long sum = 2L;
-        for(int i=1; i<=n; i++){
-            System.out.print(sum + " ");
-            sum += last;
-            last += 2L;
+        for(long i=1L; i<=n; i++){
+            System.out.print(i*i + " ");
         }
     }
 }
