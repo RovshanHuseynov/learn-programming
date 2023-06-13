47f3d618f1b5063639dfa5714f9eeb07eb971b08
rovshan.huseynov <rovsen.huseyn.oglu@gmail.com>
2023-06-13 17:28:59 +0400

    -- solve eolymp problem 8259

---
+++ src/main/java/lesson0/eolymp/Pr8259.java
@@ -0,0 +1,12 @@
+package lesson0.eolymp;
+
+import java.util.Scanner;
+
+public class Pr8259 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int k = in.nextInt();
+
+    }
+}
