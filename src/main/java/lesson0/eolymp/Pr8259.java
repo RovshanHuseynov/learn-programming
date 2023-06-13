21c36135f813c11cf2699dd5a33dd399b7943cbc
rovshan.huseynov <rovsen.huseyn.oglu@gmail.com>
2023-06-13 18:03:17 +0400

    -- solve eolymp problem 8259

--- src/main/java/lesson0/eolymp/Pr8259.java
+++ src/main/java/lesson0/eolymp/Pr8259.java
@@ -7,6 +7,28 @@ public class Pr8259 {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
         int k = in.nextInt();
+        int cnt=0;
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n; j++){
+                cnt++;
+                if(div(cnt,k)) System.out.print(".");
+                else System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
 
+    private static boolean div(int n, int k){
+        int cnt = 0;
+        if((int)Math.sqrt(n)*(int)Math.sqrt(n) == n){
+            cnt--;
+        }
+        for(int i=1; i<=n/2; i++){
+            if(n%i==0) {
+                cnt+=2;
+                if(cnt > k) return true;
+            }
+        }
+        return false;
     }
 }
