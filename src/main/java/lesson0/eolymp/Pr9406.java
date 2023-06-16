4296666645e933f4fcfc533a0007f62ad4607954
rovshan.huseynov <rovsen.huseyn.oglu@gmail.com>
2023-06-16 11:11:24 +0400

    -- solve eolymp problem 9406

---
+++ src/main/java/lesson0/eolymp/Pr9406.java
@@ -0,0 +1,13 @@
+package lesson0.eolymp;
+
+import java.util.Scanner;
+
+public class Pr9406 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int c = in.nextInt();
+        System.out.println(c / (a-b));
+    }
+}
